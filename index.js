2506ff6282c7489e75aec3847e3568e70bf761a7
Anindita Roy <anindita.roy.mu@gmail.com>
2022-05-25 10:07:21 +0600

    added loaded available time slots for a day

--- index.js
+++ index.js
@@ -19,13 +19,55 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
-    
-        app.get('/service', async(req, res) =>{
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
+
+
+
+        app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+            // step 1: get all services
+            const services = await serviceCollection.find().toArray();
+            // step 2: get the booking of that day
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+            // step 3: for each service, find bookings for that service
+            services.forEach(service => {
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                const booked = serviceBookings.map(s => s.slot);
+                const available = service.slots.filter(s => !booked.includes(s));
+                service.available = available;
+            })
+            res.send(services);
         })
+
+        /**
+         * API naming convcention
+         * app.get('/booking')  //get all bookings in this collection or get more than one or by filter
+         * app.get('/booking/:id') //get a specific booking
+         * app.get('/booking') //add a new booking
+         * app.patch('/booking/:id') //
+         * app.delete('/booking/:id') //
+         */
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists });
+            }
+            const result = bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+        });
+
+
     }
     finally {
 
@@ -33,10 +75,11 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Hello from doctor uncle!');
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Doctors App listening on port ${port}`)
 })
+
